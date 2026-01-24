b2c2f45c3bbb28b2db816b61e09d56aeb4a3148e
Caleb <caleb.hunt789@gmail.com>
2026-01-24 21:05:01 +0000

    the start is done

--- 03/02_fix_this_code/index.php
+++ 03/02_fix_this_code/index.php
@@ -4,21 +4,22 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
-$host = "localhost"
+$host = "localhost";
 
 $dbname = "week_two";
 $username = "root";
 $password = "";
 
-$dsn = "mysql:host=$hostdbname=$dbname";
+$dsn = "mysql:host=$hostdbname= $dbname";
 
 try {
     
-    $pdo = new PDO($dsn $username,);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE PDO::ERRMODE_SILENT);
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
     echo "Connected to database!";
-
-catch (PDOException $e {
-    echo "Database error: " . $e
 }
+catch (PDOException $e) {
+    echo "Database error: " . $e;
+}
+?>
--- 03/03_make_it_maintainable/index.php
+++ 03/03_make_it_maintainable/index.php
@@ -11,12 +11,7 @@ $items = ["Home", "About", "Contact"];
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My PHP Page</title>
-</head>
-<body>
+
 
 <h1>Welcome</h1>
 
